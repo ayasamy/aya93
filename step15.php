de0ec2d69fc056e5712d71671c56199e3ad11c94
ayasamy <a5656m@hotmail.com>
2018-11-22 20:31:46 +0200

    Add files via upload

---
+++ step15.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE html>
+<html dir = "rtl";>
+<head>
+<style>
+body{
+    font-family:tahoma;
+
+background-color:#CDE9F0;
+
+}
+label{
+    font-size: -webkit-xxx-large;
+    font-family: cursive;}
+form {
+    margin-right: 314px;
+    height: 328px;
+    width: 377px;
+    margin-top: 83px;
+    margin-bottom: 199px;
+    margin-left: 238px;
+    border-radius: 20px;
+    box-shadow: 2px,2px,20px #000;
+    padding-top: 70px;
+    background-color: #6DD1E3;
+    padding-left: 27px;
+    margin-right: 650x;
+}
+
+
+input[type=text], input[type=password] {
+    width: 50%;
+    hight:40%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: inline-block;
+    border: 1px solid #ccc;
+    box-sizing: border-box;
+}
+
+
+button {
+    background-color: #4CAE94;
+    color: #CDE9F0;
+    padding: 14px 20px;
+    margin: 8px 0;
+    cursor: pointer;
+    /* width: 50%; */
+    margin-left: 139px;}
+</style>
+
+</head>
+
+<body>
+<header>
+
+</header>
+
+
+<form  dir ="ltr" action="" method="post">
+
+<label> welcom you are Loged in </label>
+    
+<br />
+<div>
+•<a href="step12.php">Register a new user </a> <br /><br />
+•<a href="step14.php">Update your InFo </a> <br /><br />
+•<a href="step13.php">Your InFo</a><br /><br />
+•<a href="step11.php">Log out</a>    <br /><br /></div>
+</form>
+
+
+<footer>
+
+</footer>
+</body>
+</html>
